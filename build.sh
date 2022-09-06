f4583b5ffbb89ca13b3f883eba3a025377fcbafc
gemsvido <gemsvido@gmail.com>
2022-09-06 14:26:04 -0700

    problems occur

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 npx tsc
 cp ./src/client/index.html ./dist/client/index.html
 cp ./src/client/index.css ./dist/client/index.css
+rm userbase.json
 touch userbase.json
 echo "[]" >> userbase.json
 echo "done"
